5159cebbfe779ebd473c2758d145adfd522f7732
Chloe <chloe.swick@gmail.com>
2023-02-10 13:06:58 -0800

    added dile upload button

--- app.R
+++ app.R
@@ -24,7 +24,16 @@ ui <- fluidPage(theme = bs_theme(bootswatch = "minty"),
         mainPanel(
           tabsetPanel(
             tabPanel("Info", fluidRow(print("This app was created by Chloe, Caitlin, and Eleanor from MESM 2024. It was a long, difficult journey, but by the end, we finally figured out how to make multiple tabs and populate them. It's not about the destination but about the Shiny apps you make along the way. Peace.")
-                                      )),
+                                      ),
+                     fluidPage(
+                       
+                       # Copy the line below to make a file upload manager
+                       fileInput("file", label = h3("File input")),
+                       
+                       hr(),
+                       fluidRow(column(4, verbatimTextOutput("value")))
+                       
+                     )),
             tabPanel("Land Cover Map", verbatimTextOutput("summary")),
             tabPanel("Land Transformations", tableOutput("table"))
           )
