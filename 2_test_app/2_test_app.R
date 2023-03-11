226524b2cc9b2d472305b1849bbc67d341370810
Chloe <chloe.swick@gmail.com>
2023-03-11 12:04:44 -0800

    made roi.gpkg and tried to incorporate into upload functionality in test app 2

--- 2_test_app/2_test_app.R
+++ 2_test_app/2_test_app.R
@@ -27,7 +27,7 @@ ui <- fluidPage(
     sidebarPanel(
       fileInput("poly", "Choose spatial polygon vector file",
                 multiple = FALSE,
-                accept = c(".sqlite", ".kml", ".kmz", ".shp"))
+                accept = c(".gpkg"))
       ), # end sidebar panel
       
     mainPanel(
@@ -42,64 +42,28 @@ ui <- fluidPage(
 server <- function(input, output) {
  
 roi_poly <- reactive({
-  req(input$poly)
-  (read_sf(input$poly$datapath))
+  #req(input$poly)
+  (read_sf(input$poly$datapath)) %>% 
+    st_transform(st_crs(hawaii_rast))
       #st_zm() %>% 
       
   })
   
-  output$base_map <- renderPlot({
     
-      req(input$poly)
+     
     
-    plot(roi_poly())
+    output$base_map <- renderTmap({
       
-      # rast_reproj <- projectRaster(spat_interp()$rast, crs="+proj=longlat +datum=WGS84", method='ngb')
-      # poly_reproj <- st_transform(spat_preproc()$poly, crs="+proj=longlat +datum=WGS84")
-      # 
-      # tm_shape(lc_rast) +
-      #   tm_raster(style = "cat", 
-      #             palette = c("lightblue", "blue", "lightpink", "coral1", "red", "darkred", "tan", "darkgreen", 
-      #                                    "darkgoldenrod3", "darkkhaki", "khaki1", "brown", "lightcyan", "lightseagreen"), 
-      #                                    labels = c("NA", "Open Water", "Developed, Open Space", "Developed, Low Intensity", 
-      #                                               "Developed, Medium Intensity", "Developed, High Intensity", "Barren Land (Rock/Sand/Clay)", 
-      #                                               "Evergreen Forest", "Shrub/Scrub", "Grassland/Herbaceous", "Pasture/Hay", "Cultivated Crops", 
-      #                                               "Woody Wetlands", "Emergent Herbaceous Wetlands"), 
-      #             n= 14, 
-      #             title = "Land Use Type") +
-        # tm_shape(roi_poly()) +
-        # tm_borders(col = "black", lwd = 2)
+       req(input$poly)
+      tm_basemap(leaflet::providers$OpenStreetMap.Mapnik) +
+        tm_shape(roi_poly) +
+        tm_borders(col = "black", lwd = 2)
+    
+    })
       
-    }
-  )
-   
-  # # Load basemap
-  # basemap <- tm_shape(lc_rast) +
-  #   tm_raster(style = "cat", 
-  #             palette = c("lightblue", "blue", "lightpink", "coral1", "red", "darkred", "tan", "darkgreen", 
-  #                                    "darkgoldenrod3", "darkkhaki", "khaki1", "brown", "lightcyan", "lightseagreen"), 
-  #                                    labels = c("NA", "Open Water", "Developed, Open Space", "Developed, Low Intensity", 
-  #                                               "Developed, Medium Intensity", "Developed, High Intensity", "Barren Land (Rock/Sand/Clay)", 
-  #                                               "Evergreen Forest", "Shrub/Scrub", "Grassland/Herbaceous", "Pasture/Hay", "Cultivated Crops", 
-  #                                               "Woody Wetlands", "Emergent Herbaceous Wetlands"), 
-  #             n= 14, 
-  #             title = "Land Use Type")
-  # 
-  # output$base_map <- renderTmap({
-  #   basemap
-  # })
-  # 
-  # 
-  # # Add shapefile to basemap as overlay
-  # observe({
-  #   if (!is.null(user_shp())) {
-  #     overlay <- tm_shape(roi_poly()) +
-  #       tm_borders(col = "black", lwd = 6)
-  #     basemap + overlay
-  #   }
-  # })
-  
+     
 }
+
 
 
 shinyApp(ui, server)
